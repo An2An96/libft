b13765e68e1fb2bf87146190110c9438c4cc06a7
Rupert brax Schuppe <rschuppe@e2r2p2.21-school.ru>
2018-12-04 18:09:36 +0300

    complete tolower, toupper

--- libft.h
+++ libft.h
@@ -16,6 +16,14 @@
 # include <string.h>
 # include <stdlib.h>
 
+void	*ft_memset(void *str, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
 size_t	ft_strlen(const char *s);
 char	*ft_strcat(char *dest, const char *src);
 char	*ft_strncat(char *dest, const char *src, size_t n);
@@ -28,12 +36,7 @@ char	*ft_strrchr(const char *src, int ch);
 char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
-void	*ft_memset(void *str, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
