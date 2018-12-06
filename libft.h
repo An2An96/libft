9d5c2759f6e8b289b6243c4fbcfbe0c2d30f9448
Rupert brax Schuppe <rschuppe@e1r10p3.21-school.ru>
2018-12-06 16:03:06 +0300

    complete bonus part

--- libft.h
+++ libft.h
@@ -12,9 +12,19 @@
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <unistd.h>
 # include <stdio.h> // For tests
 # include <string.h>
 # include <stdlib.h>
+
+# define ABS(n)		((n < 0) ? -(n) : (n))
+
+typedef struct	s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}				t_list;
 
 //	libc
 void	*ft_memset(void *str, int c, size_t n);
@@ -62,5 +72,21 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s);
 char	**ft_strsplit(char const *s, char c);
+char	*ft_itoa(int n);
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+void	ft_putendl(char const *s);
+void	ft_putnbr(int n);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char const *s, int fd);
+
+//	bonus
+t_list	*ft_lstnew(void const *content, size_t content_size);
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstadd(t_list **alst, t_list *new);
+void	ft_lstpush(t_list **alst, t_list *new);
+void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list	*ft_lstmap(t_list *lst, t_list * (*f)(t_list *elem));
 
 #endif
