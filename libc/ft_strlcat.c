8bce3f992b74b566ebd3db892d33e51a25188ab4
Rupert brax Schuppe <rschuppe@e3r10p1.21-school.ru>
2018-12-08 17:31:53 +0300

    fix and refactor ft_strlcat

--- libc/ft_strlcat.c
+++ libc/ft_strlcat.c
@@ -14,18 +14,16 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	size_t			i;
-	unsigned int	j;
+	size_t	dest_len;
+	size_t	src_len;
 
-	i = ft_strlen(dest);
-	if (i > size)
-		return (size + ft_strlen(src));
-	j = 0;
-	while (i + j < (size - 1) && src[j] != '\0')
-	{
-		dest[i + j] = src[j];
-		j++;
-	}
-	dest[i + j] = '\0';
-	return (i + ft_strlen(src));
+	src_len = ft_strlen(src);
+	dest_len = ft_strlen(dest);
+	dest += dest_len;
+	dest_len = dest_len > size ? size : dest_len;
+	size -= dest_len;
+	while (size-- > 1 && *src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (dest_len + src_len);
 }
