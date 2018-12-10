adba0d917069a56734ec22fe05f9d2022630eb31
Rupert brax Schuppe <rschuppe@e1r10p5.21-school.ru>
2018-12-10 21:38:10 +0300

    add lstremove (not tests)

---
+++ bonus/ft_lstremove.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstremove.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rschuppe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/10 21:36:10 by rschuppe          #+#    #+#             */
+/*   Updated: 2018/12/10 21:36:11 by rschuppe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstremove(t_list **alst, t_list *el, void (*del)(void *, size_t))
+{
+	if (alst && *alst)
+	{
+		if (*alst == el)
+		{
+			*alst = (*alst)->next;
+			ft_lstdelone(el, del);
+		}
+		else if ((*alst)->next == el)
+		{
+			(*alst)->next = el->next;
+			ft_lstdelone(el, del);
+		}
+		else
+		{
+			ft_lstremove(&((*alst)->next), el, del);
+		}
+	}
+}
