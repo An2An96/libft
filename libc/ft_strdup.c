49ca5fc42e14e8c3a874585483d8603fc13eb31e
Rupert brax Schuppe <rschuppe@e1r10p5.21-school.ru>
2018-12-10 20:16:58 +0300

    update strdup

--- libc/ft_strdup.c
+++ libc/ft_strdup.c
@@ -17,16 +17,20 @@ char	*ft_strdup(const char *src)
 	int		i;
 	char	*dest;
 
-	dest = (char*)malloc((ft_strlen(src) + 1) * sizeof(char));
-	if (dest)
+	dest = NULL;
+	if (src && *src)
 	{
-		i = 0;
-		while (src[i] != '\0')
+		dest = (char*)malloc((ft_strlen(src) + 1) * sizeof(char));
+		if (dest)
 		{
-			dest[i] = src[i];
-			i++;
+			i = 0;
+			while (src[i] != '\0')
+			{
+				dest[i] = src[i];
+				i++;
+			}
+			dest[i] = '\0';
 		}
-		dest[i] = '\0';
 	}
 	return (dest);
 }
