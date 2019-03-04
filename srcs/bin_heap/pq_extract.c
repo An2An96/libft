0af31619ec633442fb64729369faf85ed14deb85
Rupert brax Schuppe <rschuppe@e1r11p4.21-school.ru>
2019-03-04 12:57:10 +0300

    Fix pq_extract

--- srcs/bin_heap/pq_extract.c
+++ srcs/bin_heap/pq_extract.c
@@ -6,7 +6,7 @@
 /*   By: rschuppe <rschuppe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 15:22:09 by wballaba          #+#    #+#             */
-/*   Updated: 2019/03/02 20:19:52 by rschuppe         ###   ########.fr       */
+/*   Updated: 2019/03/04 12:56:57 by rschuppe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	*pq_extract(t_pqueue *pqueue)
 {
 	void	*ret;
 
+	if (!pqueue->length)
+		return (NULL);
 	ret = pqueue->nodes[0].content;
 	pq_swap_node(pqueue, 0, pqueue->length - 1);
 	pqueue->length--;
