5a2262ef27d03625071cabf9e245926e0e8eb36a
Rupert brax Schuppe <rschuppe@e1r11p4.21-school.ru>
2019-03-09 20:24:42 +0300

    update pustr

--- srcs/output/ft_putstr.c
+++ srcs/output/ft_putstr.c
@@ -6,13 +6,13 @@
 /*   By: rschuppe <rschuppe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/06 13:01:27 by rschuppe          #+#    #+#             */
-/*   Updated: 2019/03/09 20:16:25 by rschuppe         ###   ########.fr       */
+/*   Updated: 2019/03/09 20:24:32 by rschuppe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putstr(char const *s)
+inline void	ft_putstr(char const *s)
 {
 	write(1, s, ft_strlen(s));
 }
